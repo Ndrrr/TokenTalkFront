5ca62fea51cb3987c17336e06c4ee0c2a879be04
isgandarovnr <nadir.isgandarov@kapitalbank.az>
2023-04-10 23:53:28 +0400

    url update

--- src/axios-conf.js
+++ src/axios-conf.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseBackUrl = 'http://192.168.100.209:8080/api';
-export const baseFrontUrl = 'http://192.168.100.209:3000';
+export const baseBackUrl = 'https://34.69.59.106:8443/api';
+export const baseFrontUrl = 'http://localhost:3000';
 axios.defaults.baseURL = baseBackUrl;
 
