aed58d366901d415e7f503ecc2a50e02c3601d0b
isgandarovnr <nadir.isgandarov@kapitalbank.az>
2023-04-11 09:20:44 +0400

    added domain

--- src/axios-conf.js
+++ src/axios-conf.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseBackUrl = 'https://34.69.59.106:8443/api';
+export const baseBackUrl = 'https://tokentalk.live:8443/api';
 export const baseFrontUrl = 'http://localhost:3000';
 axios.defaults.baseURL = baseBackUrl;
 
